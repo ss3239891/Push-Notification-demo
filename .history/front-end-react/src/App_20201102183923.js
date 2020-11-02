7e64012271775c1e89efca53a556e6f52a3048d1
saurav sikarwar <ss3239891@gmail.com>
2020-11-02 19:40:28 +0530

    push-notification

---
+++ .history/front-end-react/src/App_20201102183923.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import PushNotificationDemo from "./PushNotificationDemo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Web Push notification </h1>
+ 
+      <PushNotificationDemo />
+    </div>
+  );
+}
+
+export default App;
